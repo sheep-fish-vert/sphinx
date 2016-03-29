fc32ab2f7a82175537bed2059ac57552ed8ffd06
Dmitry <dmitry1939@gmail.com>
2016-03-29 16:59:49 +0300

    header 1360 done

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -1,6 +1,37 @@
 try{
 
+    // header script
+    function headerScript(){
+
+        $(document).on('click', function(e){
+            if($(e.target).is('.sendwich') || $(e.target).parents('.sendwich').length != 0){
+                if(!$('.sendwich').is('.active')){
+                    $('.sendwich').addClass('active');
+                    $('.header-nav').addClass('active');
+                }
+                else{
+                    $('.sendwich').removeClass('active');
+                    $('.header-nav').removeClass('active');
+                }
+            }
+            else if(!$(e.target).is('.sendwich') && $(e.target).parents('.sendwich').length == 0 && !$(e.target).is('.header-nav') && $(e.target).parents('.header-nav').length == 0){
+                $('.sendwich').removeClass('active');
+                $('.header-nav').removeClass('active');
+            }
+        });
+
+        $(window).resize(function(){
+            if($(window).width() > 1360){
+                $('.sendwich').removeClass('active');
+                $('.header-nav').removeClass('active');
+            }
+        });
+
+    }
+
     $(document).ready(function(){
+
+        headerScript();
 
     });
 
