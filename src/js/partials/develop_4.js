626a5238f559dcc9a3df1adbe3c5c8bd8d9c908c
Radiksman <radion91@gmail.com>
2016-03-31 11:37:00 +0300

    no message

--- src/js/partials/develop_4.js
+++ src/js/partials/develop_4.js
@@ -1,11 +1,53 @@
 try{
 
+    function scrollMainPage(){
+        if( $(window).width() > 1366 && $(window).height() > 750){
+            jQuery.easing['jswing'] = jQuery.easing['swing'];
+            jQuery.extend( jQuery.easing,
+            {
+                def: 'easeOutQuad',
+                swing: function (x, t, b, c, d) {
+                    return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+                },
+                easeInCubic: function (x, t, b, c, d) {
+                    return c*(t/=d)*t*t + b;
+                },
+                easeOutCubic: function (x, t, b, c, d) {
+                    return c*((t=t/d-1)*t*t + 1) + b;
+                },
+                easeInOutCubic: function (x, t, b, c, d) {
+                    if ((t/=d/2) < 1) return c/2*t*t*t + b;
+                    return c/2*((t-=2)*t*t + 2) + b;
+                }
+            });
+
+            var scrollPos = $(window).scrollTop(),
+                index = null,
+                timer = 0;
+
+            console.log('scrollPos ' , scrollPos);
+
+            $(window).on('mousewheel DOMMouseScroll', function(event) {
+
+                if( timer == 1 ){
+                    event.preventDefault();
+                }else{
+                    if( event.originalEvent.wheelDelta < 0){
+                        console.log('scroll down');
+                    }else if( event.originalEvent.wheelDelta > 0 ){
+                        console.log('scroll up');
+                    }
+                }
+            });
+        }
+    }
+
     $(document).ready(function(){
 
     });
 
     $(window).load(function(){
-
+        scrollMainPage();
     });
 
     $(window).resize(function(){
